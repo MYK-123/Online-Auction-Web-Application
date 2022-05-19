ea69f8cdea661addff9065d9d6caf240071692d2
MYK-123 <65278517+MYK-123@users.noreply.github.com>
2022-05-20 00:19:01 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,15 @@
 
 export FLASK_APP=src/app
 export FLASK_DEBUG=True
+export FLASK_ENV=development
+export FLASK_RUN_HOST=localhost
+export FLASK_RUN_PORT=80
 
 
-flask init_db
-flask run
+python3 -m flask init_db
+
+python3 -m flask run
+
+#flask init_db
+#flask run
 
