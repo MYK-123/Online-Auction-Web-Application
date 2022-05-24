c85a8c354625025066dc60ba8c4c447f6ffd45f1
MYK-123 <65278517+MYK-123@users.noreply.github.com>
2022-05-24 19:34:48 +0530

    Add files via upload

--- src/bot/proc_bot.py
+++ src/bot/proc_bot.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 
+from src.connectivity import loopbody1
+from src.connectivity import loopbody2
+from src.connectivity import loopbody3
+
 from multiprocessing import Process, Value
 
-class BotProcess:
-	def __init__(self):
-		pass
+class Bot(Process):
+	is_running = False
+	def run(self):
+		while is_running:
+			loopbody1()
+			loopbody2()
+			loopbody3()
+
+	def start(self):
+		is_running = True
+		super.start()
+
+	def set_deamon(self, d):
+		self.deamon = d
+
 
