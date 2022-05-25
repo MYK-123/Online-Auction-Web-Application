464b28b3b760a1ee2e4141db00a7e15484cb5329
MYK-123 <65278517+MYK-123@users.noreply.github.com>
2022-05-25 16:24:12 +0530

    Add files via upload

--- src/participate.py
+++ src/participate.py
@@ -1,5 +1,7 @@
  #!/usr/bin/python3
 
+import os
+
 from flask import g
 from flask import Blueprint
 from flask import url_for
@@ -9,9 +11,12 @@ from flask import render_template
 
 from src.auth import login_required
 
+from src.constants import UPLOAD_FOLDER
+
 from src.connectivity import make_bid
 from src.connectivity import cancel_bid
 from src.connectivity import get_auction_details
+from src.connectivity import get_auctions_list
 
 bp = Blueprint('participate', __name__)
 
@@ -19,6 +24,24 @@ def finalP(amt):
 	charges = (5 / 100) * amt #  5% of amt
 	famt = amt - charges
 	return famt
+
+def get_files(auction_id):
+	l = []
+	req_id = get_auction_details(auction_id)['request_id']
+	user_id = g.user.get_uid()
+	path = os.path.join(os.path.join(UPLOAD_FOLDER, str(user_id)), str(req_id))
+	for i, f in enumerate(os.list_dir(path)):
+		x = []
+		x.append(i)
+		x.append(f)
+		l.append(x)
+	return l
+
+
+@bp.route('/participate/', methods=['GET', 'POST'])
+@login_required
+def participate():
+	return render_template('participate_1.html', name=g.user.get_username(), items=get_auctions_list())
 
 
 @bp.route('/participate/<int:auction_id>/', methods=['GET', 'POST'])
@@ -28,13 +51,13 @@ def bid(auction_id):
 	auction_details = get_auction_details(auction_id)
 	if request.method == 'POST':
 		qty = request.form['qty']
-		ppi = request.form['ppi']
+		ppi = request.form['price']
 		amt = request.form['amt']
 		bid_id = make_bid(bidder_id, auction_id, qty, ppi, amt, finalP(amt))
-		return render_template('participate.html', qty=qty, ppi=ppi, amt=amt, details=auction_details, name=g.user.get_username())
-	return render_template('participate.html', details=auction_details, name=g.user.get_username())
+		return render_template('participate.html', qty=qty, ppi=ppi, amt=amt, auction=auction_details, name=g.user.get_username(), files=get_files(auction_id))
+	return render_template('participate.html', auction=auction_details, name=g.user.get_username(), files=get_files(auction_id))
 
-@bp.route('/participate/<int:auction_id>/bid/', methods=['GET', 'POST'])
+@bp.route('/participate/<int:auction_id>/cancel_bid/', methods=['GET', 'POST'])
 @login_required
 def cancel_bid(auction_id):
 	cancel_bid(g.user.get_uid(), auction_id)
