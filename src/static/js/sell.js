c4eb5b4256e9bc7557bad604cb8d58d9dba63c44
MYK-123 <65278517+MYK-123@users.noreply.github.com>
2022-05-25 16:22:45 +0530

    Add files via upload

--- src/static/js/sell.js
+++ src/static/js/sell.js
@@ -1,14 +1,21 @@
 function less() {
-    q = document.getElementById('qty')
-    v = parseInt(q.value) - 1
+    q = document.getElementById('qty');
+    v = parseInt(q.value) - 1;
     if (v < 0) {
-        v = 0
+        v = 0;
     }
-    q.value = v
+    q.value = v;
 }
 
 function more() {
-    q = document.getElementById('qty')
-    v = parseInt(q.value) + 1
-    q.value = v
+    q = document.getElementById('qty');
+    v = parseInt(q.value) + 1;
+    q.value = v;
+}
+
+function txtChanged () {
+q = document.getElementById("qty").value;
+p = document.getElementById("price").value;
+v = parseInt(q) * parseInt(p)
+document.getElementById("amt").value = v
 }
