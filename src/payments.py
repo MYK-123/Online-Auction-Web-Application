f1a988045fd5b2fc776748941d2108be4242b9c3
MYK-123 <65278517+MYK-123@users.noreply.github.com>
2022-05-29 01:40:04 +0530

    Update payments.py

--- src/payments.py
+++ src/payments.py
@@ -14,7 +14,7 @@ from src.connectivity import create_payment
 from src.connectivity import getBidInfo
 from src.connectivity import get_trans_list
 from src.connectivity import get_trans_history
-from src.connectivity import generate_s_order_id(auction_id)
+from src.connectivity import generate_s_order_id
 
 from src.connectivity.db import get_payout_list
 from src.connectivity.db import create_payout
@@ -41,7 +41,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 def payout_pay(auction_id, tid):
 	seller_id = get_seller_by_auction_id(auction_id)
-	sendPayoutMsg(g.user.get_uid(), seller_id, 'Payout', "Payout done successfully. (Not really for more information see project report.)")
+	send_message(g.user.get_uid(), seller_id, 'Payout', "Payout done successfully. (Not really for more information see project report.)")
 	order_id = generate_s_order_id(auction_id)
 	create_payout(tid, order_id)
 	update_payout(tid, 'TEMP_TRANS_ID', 'SUCCESS')
