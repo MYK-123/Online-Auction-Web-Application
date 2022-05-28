b37eb1bde8654326231a9b033d585f646ed8e76f
MYK-123 <65278517+MYK-123@users.noreply.github.com>
2022-05-29 00:28:49 +0530

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -10,13 +10,14 @@ from flask import request
 from flask import g
 from flask import redirect
 from flask import url_for
-from flask import send_from_directory
 
 from flask.blueprints import Blueprint
 
 from werkzeug.utils import secure_filename
 
 from src.constants import UPLOAD_FOLDER
+from src.constants import ALLOWED_EXTENTIONS
+
 from src.auth import login_required
 
 from src.connectivity import create_auction_requst
@@ -26,15 +27,12 @@ from src.connectivity import get_requests_list_by_id_both
 
 bp = Blueprint('dashboard', __name__)
 
-def get_auction_table():
-	msg = get_auctions_list()
-	return msg
 
 @bp.route('/dashboard/', methods=['GET', 'POST'])
 @login_required
 def dashboard():
 	r = g.user.get_username()
-	l = get_auction_table()
+	l = get_auctions_list()
 	return Markup.unescape(render_template('dashboard.html', name=r, items=l, rowname='t2',role=g.user.get_role()))
 
 
@@ -53,11 +51,15 @@ def sell():
 		return redirect(url_for('dashboard.dashboard'))
 	return render_template('sell.html', name=user_id,role=g.user.get_role())
 
+def is_allowed(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENTIONS
+
 def save_uploaded_files(file,user_id, req_id):
-	filename = secure_filename(file.filename)
-	path = os.path.join(os.path.join(UPLOAD_FOLDER, str(user_id)), str(req_id))
-	os.makedirs(path, exist_ok=True)
-	file.save(os.path.join(path, filename))
+	if file and is_allowed(file.filename):
+		filename = secure_filename(file.filename)
+		path = os.path.join(os.path.join(UPLOAD_FOLDER, str(user_id)), str(req_id))
+		os.makedirs(path, exist_ok=True)
+		file.save(os.path.join(path, filename))
 
 @bp.route('/requests/status/', methods=['GET', 'POST'])
 @login_required
